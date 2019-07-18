88eb2b91c46070de894a2b4c248d73e9b19d9432
Upadhyay <prayag.upd@gmail.com>
2019-07-18 11:53:38 -0700

    does not work

--- onlywallet-backend/app.js
+++ onlywallet-backend/app.js
@@ -35,6 +35,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/cache', routes.cache);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- onlywallet-backend/routes/index.js
+++ onlywallet-backend/routes/index.js
@@ -7,8 +7,23 @@ exports.index = function(req, res){
   res.render('index', { title: 'Only Wallet' });
 };
 
+exports.cache = function(req, res){
+  console.log("cache ")
+  var s = syncAddSession(res)
+  res.render('index', { title: 'Only Wallet' });
+};
+
+
+
 var Memcached = require('memcached');
 var memcached = new Memcached({'localhost:11211':1});
+
+async function syncAddSession(response){
+  var res = await addSession
+  console.log("res")
+  response.setHeader('session', 'test')
+  return res
+}
 
 function addSession(){
   memcached.add('user-id1', JSON.stringify({"session_id": "gedifp-001"}), 10, function (err) {
@@ -36,3 +51,4 @@ function addSession(){
 }
 
 addSession();
+
